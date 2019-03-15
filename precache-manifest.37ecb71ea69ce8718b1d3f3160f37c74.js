eda693e693f7f3cfe8650b1cd1e7744185a48e76
ram <ramkrishan@kontikilabs.com>
2019-03-16 02:26:16 +0530

    Updates

---
+++ precache-manifest.37ecb71ea69ce8718b1d3f3160f37c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d46cad9725efa5390d4",
+    "url": "/Emoji-Search-WithoutPagination/static/js/runtime~main.1d46cad9.js"
+  },
+  {
+    "revision": "da546a51cd0ed8f65f25",
+    "url": "/Emoji-Search-WithoutPagination/static/js/main.da546a51.chunk.js"
+  },
+  {
+    "revision": "811eb7a3c5bbfbcc50b6",
+    "url": "/Emoji-Search-WithoutPagination/static/js/2.811eb7a3.chunk.js"
+  },
+  {
+    "revision": "da546a51cd0ed8f65f25",
+    "url": "/Emoji-Search-WithoutPagination/static/css/main.f9597d08.chunk.css"
+  },
+  {
+    "revision": "811eb7a3c5bbfbcc50b6",
+    "url": "/Emoji-Search-WithoutPagination/static/css/2.12572155.chunk.css"
+  },
+  {
+    "revision": "82a18dfe5327bb1ba7b2613ebb7e4b27",
+    "url": "/Emoji-Search-WithoutPagination/index.html"
+  }
+];
